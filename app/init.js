6f04eacea3ac98e2808f69dcbcec49a1ba9763bc
Nick Denry <nick.denry@gmail.com>
2020-05-28 19:09:57 +0300

    Version bump

--- app/init.js
+++ app/init.js
@@ -3,7 +3,7 @@ let angApp = angular.module('app', [])
 
 angApp.constant('AppInfo', {
     APP_NAME: 'Chimeverse',
-    APP_VERSION: 'v0.1.53',
+    APP_VERSION: 'v0.1.54',
     APP_HOME: 'https://github.com/nick-denry/Chimeverse',
     APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/nick-denry/Chimeverse/releases',
     APP_RELEASES_URL: 'https://github.com/nick-denry/Chimeverse/releases'
