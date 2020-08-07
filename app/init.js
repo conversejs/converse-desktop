b0e42d98ac3083005e4142d8236094f4bb356c47
Nick Denry <nick.denry@gmail.com>
2020-08-07 15:49:44 +0300

    Bump project version

--- app/init.js
+++ app/init.js
@@ -3,7 +3,7 @@ let angApp = angular.module('app', [])
 
 angApp.constant('AppInfo', {
     APP_NAME: 'Converse Desktop',
-    APP_VERSION: 'v0.0.1',
+    APP_VERSION: 'v0.1.0',
     APP_HOME: 'https://github.com/conversejs/converse-desktop',
     APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/conversejs/converse-desktop/releases',
     APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases'
