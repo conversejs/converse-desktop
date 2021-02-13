8f106c65ece4519f9a7127b9529c5cd0b869d3a6
Keith Maika <keithm@aoeex.com>
2021-02-12 22:01:52 -0500

    Update version information in about screen.

--- app/init.js
+++ app/init.js
@@ -6,7 +6,9 @@ angApp.constant('AppInfo', {
     APP_VERSION: 'v0.1.0',
     APP_HOME: 'https://github.com/conversejs/converse-desktop',
     APP_RELEASES_CHECK_URL: 'https://api.github.com/repos/conversejs/converse-desktop/releases',
-    APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases'
+    APP_RELEASES_URL: 'https://github.com/conversejs/converse-desktop/releases',
+    CONVERSE_VERSION: 'v7.0.4',
+    ELECTRON_VERSION: '11.2.3'
 });
 
 module.exports = angApp
