f0e614234fd39feb7afa62adb6ca6e77039bd0a5
Keith Maika <keithm@aoeex.com>
2025-06-10 07:36:29 -0400

    Apply changes to show_self_in_roster immediately.

--- app/converse-plugins/desktop-settings.js
+++ app/converse-plugins/desktop-settings.js
@@ -1,11 +1,15 @@
 /* global api */
 
 converse.plugins.add('converse-desktop-settings', {
-    initialize () {
-        const { _converse } = this;
+    initialize() {
+        const {_converse} = this;
         api.settings.changed(function (key, newValue) {
-            if (['omemo_default'].indexOf(key) !== -1) {
+            if ('omemo_default' === key) {
                 _converse.api.settings.set(key, newValue);
+            }
+            if ('show_self_in_roster' === key) {
+                _converse.api.settings.set(key, newValue);
+                document.querySelector('converse-roster')?.requestUpdate();
             }
         });
     }
